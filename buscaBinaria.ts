99cbdc9d0c0f54ae94e36c746c69b8ac558006a8
Gerson Santiago <gersondossantossantiago@gmail.com>
2026-01-19 22:08:05 -0300

    feat: implementa a função de busca binária em TypeScript.

---
+++ buscaBinaria.ts
@@ -0,0 +1,25 @@
+// @ts-nocheck
+function buscaBinaria(arr: number[], target: number) {
+    let inicio: number = 0
+    let fim: number = arr.length - 1
+    while (inicio <= fim) {
+        let meio = Math.floor((inicio + fim) / 2)
+        if (arr[meio] === target) {
+            return meio
+        }
+        if (arr[meio] < target) {
+            inicio = meio + 1
+        }
+        else {
+            fim = meio - 1
+        }
+    }
+    return -1
+}
+
+const lista: number[] = [7, 2, 1, 3, 4, 6, 5];
+const target: number = 5;
+
+const listaOrdenada: number[] = lista.sort((a, b) => a - b)
+
+console.log(buscaBinaria(listaOrdenada, target));

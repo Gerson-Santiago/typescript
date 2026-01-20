01054a4ab2f01e74ca0b1109dd95923b76b7042a
Gerson Santiago <gersondossantossantiago@gmail.com>
2026-01-19 21:20:51 -0300

    feat: Implementa o algoritmo TwoSum e configura o ambiente TypeScript com um script de execução.

---
+++ TwoSum.ts
@@ -0,0 +1,13 @@
+// @ts-nocheck
+function twoSum(nums: number[], target: number): number[] {
+    const numMap = new Map<number, number>();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement)!, i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return [];
+}
+console.log(twoSum([2, 7, 11, 15, 9], 9));
